614074009a4a69da53c9caf83e88de3488a8fccb
sachin <sachinshrestha321@gmail.com>
2025-09-02 15:58:07 +1000

    Added encryption function

--- Question1/Q1.py
+++ Question1/Q1.py
@@ -1,3 +1,20 @@
+#Function to Encrypt Text
+def encrypt(text, shift1, shift2):   
+    result = ""
+    for ch in text:                 
+        if ch.isalpha():             
+            start = ord("a") if ch.islower() else ord("A")
+            if 'a' <= text <= 'm':
+                result += chr((ord(ch) - start + (shift1 * shift2)) % 26 + start)
+            elif 'n' <= text <= 'z':
+                result += chr((ord(ch) - start - (shift1 + shift2)) % 26 + start)
+            elif 'A' <= text <= 'M':
+                result += chr((ord(ch) - start - shift1) % 26 + start)
+            elif 'N' <= text <= 'Z':
+                result += chr((ord(ch) - start + (shift2**2)) % 26 + start)
+        else:
+            result += ch
+    return result
 shift1 = int(input("Enter shift1: "))
 shift2 = int(input("Enter shift2: "))
 
