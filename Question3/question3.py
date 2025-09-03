3ac6dd6a4ebc76ce988a64c027ac120e3461ca28
JUNG <jung@BishalJungChhetri.local>
2025-09-03 12:56:37 +1000

    Creating TURTLE obj for drawing

--- Question3/question3.py
+++ Question3/question3.py
@@ -5,6 +5,10 @@ def main():
     screen.title("Recursive Geometric Pattern Generator")
     screen.bgcolor("blue")
     screen.setup(width=800, height=800)
+    
+    t = turtle.Turtle()
+    t.speed(0)
+    t.penup()
 
 if __name__ == "__main__":
     main()
