4435af98ebfaac4d5a8016657fac0cc2f43a474c
JUNG <jung@BishalJungChhetri.local>
2025-09-03 12:55:19 +1000

    Setting up the screen

--- Question3/question3.py
+++ Question3/question3.py
@@ -1,7 +1,10 @@
 import turtle
 
 def main():
-    # Our code will go here guyz
+    screen = turtle.Screen()
+    screen.title("Recursive Geometric Pattern Generator")
+    screen.bgcolor("blue")
+    screen.setup(width=800, height=800)
 
 if __name__ == "__main__":
     main()
